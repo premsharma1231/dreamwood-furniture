8b3c63b1e1732cd60df3fbb38a12aa7aa53d725b
Purvish Haresh Sharma <premsharma786zx@gmail.com>
2026-04-09 15:04:18 +0530

    Fixed Tailwind setup and styling issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+
+base: process.env.NODE_ENV === "production"
+? "/dreamwood-furniture/"
+: "/",
+
+plugins: [react()],
+
 })
