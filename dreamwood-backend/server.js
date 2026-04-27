a4e5db922bbde27e95c2eddb77a6b778b72b0d26
Purvish Haresh Sharma <premsharma786zx@gmail.com>
2026-04-27 17:46:56 +0530

    Updated code with new features/fixes

--- dreamwood-backend/server.js
+++ dreamwood-backend/server.js
@@ -1,29 +1,34 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-import authRoutes from "./routes/auth.js";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.js";
+import productRoutes from "./routes/productRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 // MongoDB connect
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected ✅"))
-.catch(err => console.log(err));
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch(err => console.log(err));
 
 // test route
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
-const productRoutes = require("./routes/productRoutes");
-app.use("/api/products", productRoutes);
+// PORT FIX (IMPORTANT FOR RENDER)
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
