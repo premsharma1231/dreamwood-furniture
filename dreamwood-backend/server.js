58c6b8bb7a0650ff51f44621a82d3475e7abdf16
Purvish Haresh Sharma <premsharma786zx@gmail.com>
2026-04-27 07:25:40 +0530

    Updated code with new features/fixes

--- dreamwood-backend/server.js
+++ dreamwood-backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
@@ -8,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connect
-mongoose.connect("mongodb+srv://premsharma786zx:QAZQAZqaz11!!@dreamwoodfurniture.lektosl.mongodb.net/?appName=DreamwoodFurniture")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch((err) => console.log(err));
 
@@ -17,9 +18,9 @@ app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
+const productRoutes = require("./routes/productRoutes");
+app.use("/api/products", productRoutes);
+
 app.listen(5000, () => {
   console.log("Server started on port 5000");
 });
-
-const productRoutes = require("./routes/productRoutes");
-app.use("/api/products", productRoutes);
